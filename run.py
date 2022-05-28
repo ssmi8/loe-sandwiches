41356422c6d2b56eb021da9c615f011a0f058abb
ssmi8 <smithsteve8@hotmail.com>
2022-05-28 13:17:46 +0000

    Remove double validation function

--- run.py
+++ run.py
@@ -24,28 +24,29 @@ def get_sales_data():
         print("Please enter sales data from the last market.")
         print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60\n")
-        
+
         data_str = input("Enter your data here:\n")
-    
+
         sales_data = data_str.split(",")
-        validate_data(sales_data)
 
         if validate_data(sales_data):
             print("Data is valid!")
             break
-    return sales_data        
+
+    return sales_data
+
 
 def validate_data(values):
     """
     Inside the try, converts all string values into integers.
     Raises ValueError if strings cannot be converted into int,
-    or if ther aren't exactly 6 values.
+    or if there aren't exactly 6 values.
     """
     try:
         [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
-                F"Exactly 6 values required, you provided {len(values)}"
+                f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
@@ -53,9 +54,10 @@ def validate_data(values):
 
     return True
 
+
 def update_worksheet(data, worksheet):
     """
-    Recieves a list of integers to be inserted into a worksheet
+    Receives a list of integers to be inserted into a worksheet
     Update the relevant worksheet with the data provided
     """
     print(f"Updating {worksheet} worksheet...\n")
@@ -63,15 +65,15 @@ def update_worksheet(data, worksheet):
     worksheet_to_update.append_row(data)
     print(f"{worksheet} worksheet updated successfully\n")
 
+
 def calculate_surplus_data(sales_row):
     """
     Compare sales with stock and calculate the surplus for each item type.
-
-    The surplus is defined as the sales figure subtracted fromt he stock:
+    The surplus is defined as the sales figure subtracted from the stock:
     - Positive surplus indicates waste
-    - Negative surplus indicates extra made when stock ran out.
+    - Negative surplus indicates extra made when stock was sold out.
     """
-    print("Calculating suprlus data...\n")
+    print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
     
@@ -79,23 +81,25 @@ def calculate_surplus_data(sales_row):
     for stock, sales in zip(stock_row, sales_row):
         surplus = int(stock) - sales
         surplus_data.append(surplus)
-    
+
     return surplus_data
+
 
 def get_last_5_entries_sales():
     """
     Collects columns of data from sales worksheet, collecting
-    the last 5 entires for each sandwich and returns data
+    the last 5 entries for each sandwich and returns the data
     as a list of lists.
     """
     sales = SHEET.worksheet("sales")
 
     columns = []
-    for ind in range(1,7):
+    for ind in range(1, 7):
         column = sales.col_values(ind)
         columns.append(column[-5:])
-    
+
     return columns
+
 
 def calculate_stock_data(data):
     """
@@ -109,7 +113,7 @@ def calculate_stock_data(data):
         average = sum(int_column) / len(int_column)
         stock_num = average * 1.1
         new_stock_data.append(round(stock_num))
-    
+
     return new_stock_data
 
 
@@ -121,7 +125,7 @@ def main():
     sales_data = [int(num) for num in data]
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_worksheet(new_surplus_data, "surplus")    
+    update_worksheet(new_surplus_data, "surplus")
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
@@ -129,4 +133,3 @@ def main():
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
-
